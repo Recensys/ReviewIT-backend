a0c4a8ee1dd4f27b6f2cab04414d1702bb0fff3d
mk_ti <mk_titan@hotmail.com>
2016-10-03 10:11:49 +0200

    tested and impl update delete

---
+++ ReviewIT-Backend/RecensysCoreRepository.Tests/Unittests/StudyConfigRepository.Tests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Moq;
+using RecensysCoreRepository.DTOs;
+using RecensysCoreRepository.EF;
+using RecensysCoreRepository.Entities;
+using RecensysCoreRepository.Repositories;
+using Xunit;
+
+namespace RecensysCoreRepository.Tests.Unittests
+{
+    public class StudyConfigRepositoryTests
+    {
+        [Fact]
+        public void Create_returns_new_id()
+        {
+            var options = Helpers.CreateInMemoryOptions();
+			var context = new RecensysContext(options);
+            var repo = new StudyConfigRepository(context);
+            var dto = new StudyConfigDTO();
+
+
+            using (repo)
+            {
+                var id = repo.Create(dto);
+
+				Assert.Equal(1,id);
+            }
+        }
+
+        [Fact]
+        public void Create_calls_saveChanges_on_context()
+        {
+            var mock = new Mock<IRecensysContext>();
+            mock.Setup(m => m.Studies.Add(It.IsAny<Study>()));
+            var repo = new StudyConfigRepository(mock.Object);
+            var dto = new StudyConfigDTO();
+
+            using (repo)
+            {
+                var id = repo.Create(dto);
+            }
+
+			mock.Verify(m => m.SaveChanges(), Times.Once);
+        }
+
+        [Fact]
+        public void Read_id_returnes_study_with_id()
+        {
+            var options = Helpers.CreateInMemoryOptions();
+            var context = new RecensysContext(options);
+            var repo = new StudyConfigRepository(context);
+            context.Studies.Add(new Study() {Id = 1, Name = "Name", Description = "Desc"});
+            context.Studies.Add(new Study() {Id = 2, Name = "Name2", Description = "Desc"});
+            context.SaveChanges();
+
+            using (repo)
+            {
+                var dto = repo.Read(2);
+
+				Assert.Equal("Name2", dto.Name);
+				Assert.Equal("Desc", dto.Description);
+				Assert.Equal(2, dto.Id);
+            }
+        }
+
+        [Fact]
+        public void Update_dto_with_name_repo_updated_correctly()
+        {
+            var options = Helpers.CreateInMemoryOptions();
+            var context = new RecensysContext(options);
+            var repo = new StudyConfigRepository(context);
+            var entity = new Study() {Id = 1, Name = "", Description = "Desc"};
+            context.Studies.Add(entity);
+            context.SaveChanges();
+
+            using (repo)
+            {
+                var dto = new StudyConfigDTO() {Id = 1, Name = "Name"};
+
+                repo.Update(dto);
+
+                Assert.Equal("Name", entity.Name);
+            }
+        }
+
+        [Fact]
+        public void Delete_stored_item_with_id_1_removed()
+        {
+            var options = Helpers.CreateInMemoryOptions();
+            var context = new RecensysContext(options);
+            var repo = new StudyConfigRepository(context);
+            var entity = new Study() { Id = 1, Name = "", Description = "Desc" };
+            context.Studies.Add(entity);
+            context.SaveChanges();
+
+            using (repo)
+            {
+                var dto = new StudyConfigDTO() {Id = 1};
+                repo.Delete(dto);
+
+                Assert.True(context.Studies.Count(s => s.Id == 1)==0);
+            }
+        }
+    }	
+}
---
+++ ReviewIT-Backend/RecensysCoreRepository/Repositories/StudyConfigRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using RecensysCoreRepository.DTOs;
+using RecensysCoreRepository.EF;
+using RecensysCoreRepository.Entities;
+using Remotion.Linq.Parsing.Structure.IntermediateModel;
+
+namespace RecensysCoreRepository.Repositories
+{
+    public class StudyConfigRepository : IStudyConfigRepository
+    {
+        private readonly IRecensysContext _context;
+
+        public StudyConfigRepository(IRecensysContext context)
+        {
+            if (context == null) throw new ArgumentNullException($"{nameof(context)} is null");
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public int Create(StudyConfigDTO dto)
+        {
+            var entity = new Study();
+            DTOMapper.Map(dto, entity);
+            _context.Studies.Add(entity);
+            _context.SaveChanges();
+            return dto.Id = entity.Id;
+        }
+
+        public StudyConfigDTO Read(int id)
+        {
+            var dtos = from s in _context.Studies
+                      where s.Id == id
+                      select new StudyConfigDTO()
+                      {
+                          Id = s.Id,
+                          Name = s.Name,
+                          Description = s.Description,
+                          Criteria = (from c in s.Criteria
+                                      select new CriteriaDTO() { Id = c.Id, Value = c.Value }).ToList(),
+                          AvailableFields = (from af in s.Fields
+                                             select new FieldDTO() { Id = af.Id, Name = af.Name, DataType = (DataType)af.DataType }).ToList(),
+                          Stages = (from st in s.Stages
+                                    select new StageConfigDTO()
+                                    {
+                                        Id = st.Id,
+                                        Name = st.Name,
+                                        Description = st.Description,
+                                        VisibleFields = (from vf in st.StageFields
+                                                         where vf.FieldType == FieldType.Visible
+                                                         select new FieldDTO() { Id = vf.Field.Id, Name = vf.Field.Name, DataType = (DataType)vf.Field.DataType }).ToList(),
+                                        RequestedFields = (from rf in st.StageFields
+                                                           where rf.FieldType == FieldType.Requested
+                                                           select new FieldDTO() { Id = rf.Field.Id, Name = rf.Field.Name, DataType = (DataType)rf.Field.DataType }).ToList(),
+                                    }).ToList(),
+                          Researchers = (from r in s.Users
+                                         select new ResearcherDetailsDTO() { Id = r.Id, FirstName = r.FirstName }).ToList()
+                      };
+            return dtos.Single();
+        }
+        
+
+        public bool Update(StudyConfigDTO dto)
+        {
+            var entity = _context.Studies.Single(s => s.Id == dto.Id);
+            DTOMapper.Map(dto, entity);
+            _context.Studies.Update(entity);
+            return _context.SaveChanges() > 0;
+        }
+
+        public bool Delete(StudyConfigDTO dto)
+        {
+            var entity = _context.Studies.Single(s => s.Id == dto.Id);
+            _context.Studies.Remove(entity);
+            return _context.SaveChanges() > 0;
+        }
+    }
+
+    
+}
