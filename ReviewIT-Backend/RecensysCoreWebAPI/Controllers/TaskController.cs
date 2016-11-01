d2b7dc4506ddb88c28d21efea0b1495c2df3f96d
mk_ti <mk_ti@DESKTOP-OVCFSSB>
2016-11-01 11:35:43 +0100

    initial

---
+++ ReviewIT-Backend/RecensysCoreWebAPI/Controllers/TaskController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RecensysCoreRepository.Repositories;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RecensysCoreWebAPI.Controllers
+{
+    [Route("api")]
+    public class TaskController : Controller
+    {
+
+        private readonly IReviewTaskRepository _tRepo;
+
+        public TaskController(IReviewTaskRepository tRepo)
+        {
+            _tRepo = tRepo;
+        }
+
+        // GET: api/values
+        [HttpGet("tasks")]
+        public IActionResult Get(int uid, int sid)
+        {
+
+            if (!ModelState.IsValid || uid == 0 || sid == 0) return BadRequest();
+
+            try
+            {
+                using (_tRepo)
+                {
+                    return Json(_tRepo.GetListDto(sid, uid));
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+    }
+}
