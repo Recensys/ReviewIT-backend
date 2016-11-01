439b499768d966d3ac9d526503c1822c84d3c2d8
mk_ti <mk_ti@DESKTOP-OVCFSSB>
2016-11-01 11:32:46 +0100

    criteria engine initial

---
+++ ReviewIT-Backend/RecensysCoreBLL/CriteriaEngine.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace RecensysCoreBLL
+{
+    public class CriteriaEngine: ICriteriaEngine
+    {
+        public int Evaluate(int stageId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<int> EvaluateAsync(int stageId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ ReviewIT-Backend/RecensysCoreBLL/ICriteriaEngine.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace RecensysCoreBLL
+{
+    public interface ICriteriaEngine
+    {
+        int Evaluate(int stageId);
+
+        Task<int> EvaluateAsync(int stageId);
+    }
+}
