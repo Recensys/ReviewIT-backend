03ad80e24eb00baf75ed49eaf6c270005e498c00
mk_ti <mk_ti@DESKTOP-OVCFSSB>
2016-11-10 10:09:49 +0100

    initial system tests

---
+++ ReviewIT-Backend/SystemTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SystemTests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a1e9b5be-552c-43b9-a485-2a472e82d9f0")]
---
+++ ReviewIT-Backend/SystemTests/Tests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using RecensysCoreRepository.DTOs;
+using RecensysCoreWebAPI;
+using Xunit;
+
+namespace SystemTests
+{
+
+    public class TestFixture : IDisposable
+    {
+
+        public TestServer TestServer { get; set; }
+        public HttpClient HttpClient { get; set; }
+        public string StudyId { get; set; }
+        public string StageId { get; set; }
+
+        public TestFixture()
+        {
+            TestServer = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+            HttpClient = TestServer.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            TestServer.Dispose();
+            HttpClient.Dispose();
+        }
+    }
+
+
+    public class Tests : IClassFixture<TestFixture>
+    {
+        private TestFixture _f;
+
+        public Tests(TestFixture fixture)
+        {
+            _f = fixture;
+        }
+
+        [Fact]
+        public async Task Study_Post_Get()
+        {
+            var dto = new StudyDetailsDTO
+            {
+                Name = "Test Study"
+            };
+
+            // Act
+            var response = await _f.HttpClient.PostAsync("api/study", new StringContent(JsonConvert.SerializeObject(dto),Encoding.UTF8,"application/json"));
+            response.EnsureSuccessStatusCode();
+            _f.StudyId = await response.Content.ReadAsStringAsync();
+
+            var getResponse = await _f.HttpClient.GetStringAsync($"api/study/{_f.StudyId}");
+            var resDto = JsonConvert.DeserializeObject<StudyDetailsDTO>(getResponse);
+
+            // Assert
+            Assert.Equal("Test Study", resDto.Name);
+        }
+
+        [Fact]
+        public async Task Source_Post()
+        {
+
+            var stream = new FileStream("bibtex100.bib", FileMode.Open);
+            var dataContent = new MultipartFormDataContent("file");
+            dataContent.Add(new StreamContent(stream));
+
+            // Act
+            var response =
+                await _f.HttpClient.PostAsync($"api/study/{_f.StudyId}/config/source", dataContent);
+            response.EnsureSuccessStatusCode();
+            var nrOfArticles = await response.Content.ReadAsStringAsync();
+            
+            // Assert
+            Assert.Equal("100", nrOfArticles);
+        
+        }
+    }
+}
